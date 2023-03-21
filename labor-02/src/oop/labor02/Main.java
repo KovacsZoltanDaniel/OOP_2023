00227460ceb459809a42be7c019ed9a1060ec258
KovacsZoltanDaniel <kovacs.zoltan@student.ms.sapientia.ro>
2023-03-21 22:51:33 +0200

    lab2

--- labor-02/src/oop/labor02/Main.java
+++ labor-02/src/oop/labor02/Main.java
@@ -1,7 +1,53 @@
 package oop.labor02;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("labor02");
+        System.out.println(DateUtil.isValidDate(2000, 2, 29));
+        System.out.println(!DateUtil.isValidDate(2000, 2, 30));
+        System.out.println(!DateUtil.isValidDate(1900, 2, 29));
+        System.out.println(DateUtil.isValidDate(1900, 2, 28));
+        System.out.println(!DateUtil.isValidDate(-1900, 2, 28));
+        System.out.println(!DateUtil.isValidDate(0, 2, 28));
+        System.out.println(!DateUtil.isValidDate(2021, 2, 29));
+        System.out.println(DateUtil.isValidDate(2020, 2, 29));
+        System.out.println(!DateUtil.isValidDate(2020, 1, 32));
+        System.out.println(!DateUtil.isValidDate(2020, 1, 0));
+        System.out.println(!DateUtil.isValidDate(2020, 0, 0));
+        System.out.println(!DateUtil.isValidDate(2020, 4, 31));
+        System.out.println(DateUtil.isValidDate(2020, 1, 31));
+
+        MyDate date1 = new MyDate(2023, 3, 1);
+        System.out.println(date1);
+
+        // b.
+        Random rand = new Random();
+
+
+        MyDate[] dates = new MyDate[1000];
+        int invalidDates = 0;
+        int valid = 0;
+        for (int i = 0; i < dates.length; i++) {
+
+            int year = 1 + rand.nextInt(2023);
+            int month = 1 + rand.nextInt(12);
+            int day = 1 + rand.nextInt(31);
+
+            dates[i] = new MyDate(year, month, day);
+
+            if (!DateUtil.isValidDate(dates[i].getYear(), dates[i].getMonth(), dates[i].getDay())) {
+                invalidDates++;
+            }
+            else valid++;
+        }
+        for (int i = 0; i < dates.length; i++) {
+            if (DateUtil.isValidDate(dates[i].getYear(), dates[i].getMonth(), dates[i].getDay())){
+                System.out.println(dates[i]);
+            }
+        }
+        System.out.println("number of invalid dates: "+invalidDates);
+        System.out.println("number of valid dates: "+valid);
+
     }
 }
---
+++ labor-02/src/oop/labor02/MyDate.java
@@ -0,0 +1,33 @@
+package oop.labor02;
+
+public class MyDate {
+    private int year;
+    private int month;
+    private int day;
+    public MyDate(int year, int month, int day){
+        if(DateUtil.isValidDate(year,month,day)){
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
+    }
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    @Override
+    public String toString() {
+        return "oop.labor02.MyDate: " +
+                "year= " + year +
+                " month= " + month +
+                " day= " + day ;
+    }
+}
