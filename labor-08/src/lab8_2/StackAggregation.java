799fc25fc405cc095244940173ccaa4510ec6ae7
KovacsZoltanDaniel <kovacs.zoltan@student.ms.sapientia.ro>
2023-04-26 14:51:08 +0300

    lab_8

--- labor-08/src/lab8_2/StackAggregation.java
+++ labor-08/src/lab8_2/StackAggregation.java
@@ -3,11 +3,12 @@ package lab8_2;
 import java.util.ArrayList;
 
 public class StackAggregation {
-    private ArrayList<Object> items = new ArrayList<>();
+    private ArrayList<Object> items ;
     private final  int capacity;
 
     public StackAggregation(int capacity) {
         this.capacity = capacity;
+        items = new ArrayList<>();
     }
     public boolean isFull(){
        return (items.size() == capacity);
@@ -22,7 +23,7 @@ public class StackAggregation {
         items.add(object);
     }
     public void pop(){
-        if(isEmpty()){
+        if(items.isEmpty()){
             return;
         }
         items.remove(items.size() - 1);
