7d655862ddaf0b81d6ae103680bf1cb052e86dd7
KovacsZoltanDaniel <kovacs.zoltan@student.ms.sapientia.ro>
2023-04-26 12:16:51 +0300

    lab_8

--- labor-08/src/lab8_2/StackInheritance.java
+++ labor-08/src/lab8_2/StackInheritance.java
@@ -2,38 +2,37 @@ package lab8_2;
 
 import java.util.ArrayList;
 
-public class StackAggregation {
-    private ArrayList<Object> items = new ArrayList<>();
-    private final  int capacity;
+public class StackInheritance extends ArrayList<Object> {
+    private final int capacity;
 
-    public StackAggregation(int capacity) {
+    public StackInheritance(int capacity) {
         this.capacity = capacity;
     }
     public boolean isFull(){
-       return (items.size() == capacity);
+        return (this.size() == capacity);
     }
     public boolean isEmpty(){
-        return (items.size() == 0);
+        return (this.size() == 0);
     }
     public void push(Object object){
         if(isFull()){
             return;
         }
-        items.add(object);
+        this.add(object);
     }
     public void pop(){
         if(isEmpty()){
             return;
         }
-        items.remove(items.size() - 1);
+        this.remove(this.size() - 1);
     }
     public Object top(){
         if(isEmpty()){
             return null;
         }
-        return items.get(items.size() -1);
+        return this.get(this.size() -1);
     }
     public int getSize(){
-        return items.size();
+        return this.size();
     }
 }
